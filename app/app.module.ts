2d5449cb3f64780b8d703ca3bc3aa1c51ff31130
Sathish G <sathish.gajendran@gmail.com>
2017-04-17 13:09:25 +0530

    Material design added

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,9 +1,14 @@
 import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
+import {MaterialModule} from '@angular/material';
+import 'hammerjs';
 import {AppComponent} from './app.component';
 
 @NgModule({
-    imports: [BrowserModule], 
+    imports: [
+        BrowserModule, 
+        MaterialModule
+    ], 
     declarations: [AppComponent], 
     bootstrap: [AppComponent]
 })
--- systemjs.config.js
+++ systemjs.config.js
@@ -18,6 +18,11 @@
       '@angular/compiler': 'npm:@angular/compiler/bundles/compiler.umd.js',
       '@angular/platform-browser': 'npm:@angular/platform-browser/bundles/platform-browser.umd.js',
       '@angular/platform-browser-dynamic': 'npm:@angular/platform-browser-dynamic/bundles/platform-browser-dynamic.umd.js',
+      '@angular/forms': 'npm:@angular/forms/bundles/forms.umd.js',
+      '@angular/http': 'npm:@angular/http/bundles/http.umd.js',
+      '@angular/animations': 'npm:@angular/animations/bundles/animations.umd.js',
+      '@angular/material': 'npm:@angular/material/bundles/material.umd.js',
+      'hammerjs': 'npm:hammerjs/hammer.js',
    
       // other libraries
       'rxjs': 'npm:rxjs'
